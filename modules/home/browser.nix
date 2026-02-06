6675d790be73b1571d525e3252ad53ab9c3b1a34
cd4u2b0z <maksed-elf-0r@icloud.com>
2026-02-05 19:56:38 -0500

    fix: use extensions.packages for librewolf (new syntax)

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -37,7 +37,7 @@
       isDefault = true;
       
       # Extensions via NUR
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         ublock-origin
         darkreader
         bitwarden
